4f8a82a3dd672ec9cf0de94c60101bae82ed6eb1
sdp-code <48664092+sdp-code@users.noreply.github.com>
2019-03-18 08:42:42 +0530

    prime numbers

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject prime_multiplication "0.1.0-SNAPSHOT"
+  :description "Print out a multiplication table of the first 10 prime numbers."
+  :url "http://github.com/tlicata/prime_multiplication"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+				;; [ch.qos.logback/logback-classic "1.1.3"]
+				 ]
+  :main prime_multiplication.core)
